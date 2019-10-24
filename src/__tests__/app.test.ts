208a6372115dcfdb3b41c3697cc12061cf49fc45
Marc Miller <marc.miller@email.saintleo.edu>
2019-10-23 22:10:31 -0400

    FizzBuzz Kata from http://codingdojo.org/kata/FizzBuzz/ completed with TDD & Typescript

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,7 +1,11 @@
-import helloWorld from "../app";
+import {fizzBuzzStageOne, fizzBuzzStageTwo} from "../app";
 
-describe('App tests go in here', () => {
-    it('should return "Hello World!"', () => {
-        expect(helloWorld()).toEqual("Hello World!")
+describe('FizzBuzz Kata tests', () => {
+    it('should return 1-15 with multiples of 3 replaced with Fizz, 5 with Buzz, 3 & 5 with FizzBuzz', () => {
+        expect(fizzBuzzStageOne(15)).toEqual([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"])
     });
+
+    it('should return as FizzBuzz, but replacing if divisible or containing 3 or 5', () => {
+        expect(fizzBuzzStageTwo(15)).toEqual([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz","Fizz",14,"FizzBuzz"])
+    })
 });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,37 @@
-export default function helloWorld(): string {
-    return 'Hello World!'
-};
+type FizzBuzzOutputType = Array<number | string>;
+
+
+export function fizzBuzzStageOne(upperLimit: number): FizzBuzzOutputType  {
+    let out: FizzBuzzOutputType = new Array<number | string>();
+
+    for (let i = 1; i <= upperLimit; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            out = out.concat('FizzBuzz')
+        } else if (i % 3 == 0) {
+            out = out.concat('Fizz')
+        } else if (i % 5 == 0) {
+            out = out.concat('Buzz')
+        } else {
+            out = out.concat(i)
+        }
+    }
+    return out
+}
+
+export function fizzBuzzStageTwo(upperLimit: number): FizzBuzzOutputType {
+    let out: FizzBuzzOutputType = new Array<number|string>();
+
+    for (let i = 1; i <= upperLimit; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            out = out.concat('FizzBuzz')
+        } else if (i % 3 == 0 || i.toString().match('[3]')) {
+            out = out.concat('Fizz')
+        } else if (i % 5 == 0 || i.toString().match('[5]')) {
+            out = out.concat('Buzz')
+        } else {
+            out = out.concat(i)
+        }
+    }
+
+    return out;
+}
