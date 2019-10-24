56ad38266072f66f394f969f52ab5bd810cd21aa
Marc Miller <marc.miller@email.saintleo.edu>
2019-10-23 22:18:08 -0400

    FizzBuzz Kata from http://codingdojo.org/kata/FizzBuzz/ completed with TDD & Typescript

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -7,5 +7,5 @@ describe('FizzBuzz Kata tests', () => {
 
     it('should return as FizzBuzz, but replacing if divisible or containing 3 or 5', () => {
         expect(fizzBuzzStageTwo(15)).toEqual([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz","Fizz",14,"FizzBuzz"])
-    })
+    });
 });
