329327ca3b798d6fb0c58b6c4a3e3405997be457
Marc Miller <marc.miller.4@us.af.mil>
2020-12-01 16:46:06 -0500

    interview with Vladimir Palko

--- src/__tests__/set.test.ts
+++ src/__tests__/set.test.ts
@@ -1,8 +1,74 @@
-import Set from "../set";
+import Set from '../set';
+
+/*
+ *
+ * Step 1 - New Set
+ * Step 2 - Add item
+ * Step 3 - Remove item
+ * Step 4 - Add a specific object
+ * Step 5 - Refactor
+ * Step 6 - Remove specific object
+ * Step 7 - Add multiple objects
+ * Step 8 - Refactor
+ * Step 9 - Remove multiple objects
+ * Step 10 - Refactor
+ * Step 11 - Ensure no duplicates
+ * ^^^^^^^ We are here ^^^^^^^
+ * Step 12 - Add any number of objects to set
+ *
+ * */
 
 describe('Set tests', () => {
-  it('should instantiate the Set class', () => {
-    const set = new Set();
+  const set = new Set();
+  const obj1 = new Object({ id: 1 });
+  const obj2 = new Object({ id: 2 });
+  const obj3 = new Object({ id: 3 });
+
+  it('should instantiate the set, and it should be empty', () => {
+    expect(set.isEmpty()).toBeTruthy();
+  });
+
+  it('should be able to add a specific object to the set', () => {
+    set.addItem(obj1);
+
+    expect(set.isEmpty()).toBeFalsy();
+    expect(set.contains(obj1)).toBeTruthy();
+  });
+
+  it('should be able to remove a specific object', () => {
+    set.removeItem(obj1)
+    
+    expect(set.isEmpty()).toBeTruthy();
+    expect(set.contains(obj1)).toBeFalsy();
   })
   
-})
+  it('should be able to add multiple objects', () => {
+    set.addMultiple([obj1, obj2, obj3])
+    
+    expect(set.isEmpty()).toBeFalsy();
+    expect(set.contains(obj1)).toBeTruthy();
+    expect(set.contains(obj2)).toBeTruthy();
+    expect(set.contains(obj3)).toBeTruthy();
+  })
+
+  it('should be able to remove multiple objects', () => {
+    set.removeMultiple([obj1, obj3]);
+
+    expect(set.isEmpty()).toBeFalsy();
+    expect(set.contains(obj1)).toBeFalsy();
+    expect(set.contains(obj2)).toBeTruthy();
+    expect(set.contains(obj3)).toBeFalsy();
+  })
+  
+  it('should fix a bug', () => {  
+    set.addItem(obj1);
+    expect(set.contains(obj1)).toBeTruthy();
+    expect(set.contains(obj2)).toBeTruthy();
+
+    set.addMultiple([obj2, obj3])
+    expect(set.contains(obj1)).toBeTruthy();
+    expect(set.contains(obj2)).toBeTruthy();
+    expect(set.contains(obj3)).toBeTruthy();
+  })
+});
+
--- src/set.ts
+++ src/set.ts
@@ -1,3 +1,48 @@
 export default class Set {
+  setItems: object[];
   
+  constructor() {
+    this.setItems = [];
+  }
+
+  isEmpty() {
+    return this.setItems.length === 0;
+  }
+
+  addItem(item: Object) {
+    this.setItems = [...this.setItems, item];
+  }
+  
+  addMultiple(items: Object[]) {
+    this.setItems = [...this.setItems, ...items];
+  }
+
+  removeItem(item: object) {
+    let newItems: object[] = [];
+
+    if (this.contains(item)) {
+      this.setItems.forEach(obj => {
+        if (obj !== item) {
+          newItems = [...newItems, obj];
+        }      
+      })
+
+      this.setItems = newItems;
+    };
+  }
+
+  removeMultiple(items: object[]) {
+    for (let item of items) {
+      this.removeItem(item);
+    }
+  }
+  
+  contains(obj: Object) {
+    for (let item of this.setItems) {
+      if (obj === item) {
+        return true
+      }
+    }
+    return false
+  }
 }
