d0b8500445acd8beb12cdcd3ffadbbae41d44e45
Marc Miller <marcjmiller@outlook.com>
2021-04-07 12:57:45 -0400

    rpa with Adam from the Navy
    Good session overall, less code but so much talk around what we were
    doing it was still a very productive time.

--- src/__tests__/set.test.ts
+++ src/__tests__/set.test.ts
@@ -1,12 +1,13 @@
 import Set from "../set";
 
 /**
+ * red -> green -> refactor
  *
  * Step 1 - New Set
- * ^^^^^^^ We are here ^^^^^^^
  * Step 2 - Add item
  * Step 3 - Remove item
  * Step 4 - Add a specific object
+ * ^^^^^^^ We are here ^^^^^^^
  * Step 5 - Refactor
  * Step 6 - Remove specific object
  * Step 7 - Add multiple objects
@@ -19,5 +20,29 @@ import Set from "../set";
  */
 
 describe("Set tests", () => {
-  
+  const set = new Set();
+
+  it("should be empty after initializing", () => {
+    expect(set.isEmpty).toBeTruthy();
+  });
+
+  it("should be able to add an item", () => {
+    set.addItem();
+    expect(set.isEmpty).toBeFalsy();
+  });
+
+  it("should be able to remove an item", () => {
+    set.removeItem();
+    expect(set.isEmpty).toBeTruthy();
+  });
+
+  it("should be able to add specific items", () => {
+    const obj1 = { id: 1, name: "Object 1" };
+    const obj2 = { id: 1, name: "Object 1" };
+
+    set.addItem(obj1);
+    expect(set.isEmpty).toBeFalsy();
+    expect(set.contains(obj1)).toBeTruthy();
+    expect(set.contains(obj2)).toBeFalsy();
+  });
 });
--- src/set.ts
+++ src/set.ts
@@ -1,3 +1,33 @@
+interface Item {
+  id: number;
+  name: string;
+}
+
 export default class Set {
-  
+  private _items: Item[] = [];
+  private _isEmpty: boolean = true;
+
+  get isEmpty() {
+    return this._isEmpty;
+  }
+
+  addItem(item?: Item) {
+    item && this._items.push(item);
+    this._isEmpty = false;
+  }
+
+  contains(item: Item) {
+    let result = false;
+    this._items.forEach((obj) => {
+      if (obj === item) {
+        result = true;
+      }
+    });
+
+    return result;
+  }
+
+  removeItem() {
+    this._isEmpty = true;
+  }
 }
